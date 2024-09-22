3603faf47bb9ec08d552b31662a43d2ea1b843e0
AndHisNameIsJohnCenaaa <vitalikfsoru@gmail.com>
2024-09-22 10:53:24 +0300

    add request parameter validation

--- src/main/java/com/neoflex/testtask/controller/CalculatorController.java
+++ src/main/java/com/neoflex/testtask/controller/CalculatorController.java
@@ -3,11 +3,13 @@ package com.neoflex.testtask.controller;
 import com.neoflex.testtask.service.VacationPayCalculateService;
 import com.neoflex.testtask.to.VacationPayTo;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +21,7 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class CalculatorController {
 
     private final VacationPayCalculateService service;
@@ -26,8 +29,8 @@ public class CalculatorController {
 
     @GetMapping("/calculate")
     public ResponseEntity<VacationPayTo> calculate(
-            @RequestParam BigDecimal averageSalary,
-            @RequestParam int vacationDaysNumber,
+            @RequestParam @Positive BigDecimal averageSalary,
+            @RequestParam @Positive int vacationDaysNumber,
             @RequestParam @Nullable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vacationStartDate) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(service.calculate(averageSalary, vacationDaysNumber, vacationStartDate));
